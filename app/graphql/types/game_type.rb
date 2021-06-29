3ab51457c4d4f04b5e97f29e715527cf49a15c06
Mutuba <danielmutubait@gmail.com>
2021-06-29 16:52:56 +0300

    Add tests for game_type definition (graphql/types/game_type.rb)

--- app/graphql/types/game_type.rb
+++ app/graphql/types/game_type.rb
@@ -4,6 +4,6 @@ module Types
   class GameType < Types::BaseObject
     field :id, ID, null: false
     field :player, String, null: false
-    field :board, String, null: false
+    field :board, BoardType, null: false
   end
 end
---
+++ spec/graphql/types/game_type_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# spec/graphql/types/game_type_spec.rb
+
+RSpec.describe Types::GameType do
+    set_graphql_type
+  
+    it 'has an :id field of ID type' do
+      # Ensure that the field id is of type ID
+      expect(subject.fields['id'].type.to_type_signature).to eq('ID!')
+    end
+  
+    it 'has a :player field of String type' do
+      # Ensure the field is of String type
+      expect(subject.fields['player'].type.to_type_signature).to eq('String!')
+    end
+  
+    it 'has a :board field of BoardType type' do
+      # Ensure the field is of BoardType type
+      expect(subject.fields['board'].type.to_type_signature).to eq('Board!')
+    end
+  
+  end
+  
